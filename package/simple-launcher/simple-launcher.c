c74ead4ae1289470077dc73e0a0fb16c3ad90ae2
Hao Nguyen <hao.ict56@gmail.com>
2024-06-23 09:24:31 +0900

    Update simple-launcher

--- package/simple-launcher/simple-launcher.c
+++ package/simple-launcher/simple-launcher.c
@@ -5,6 +5,11 @@
 #define MAX_COMMAND_LENGTH 256
 #define MAX_NAME_LENGTH 256
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const char *FONT_PATH = "./simple-launcher.ttf";
+const char *COMMANDS_FILE = "./simple-launcher-commands.txt";
+
 SDL_Window *window;
 SDL_Renderer *renderer;
 SDL_Joystick *joystick;
@@ -32,7 +37,7 @@ char volume[8];
 char volumeDisplayString[20] = "Volume: 0%";
 char creditDisplayString[MAX_NAME_LENGTH] = "Buildroot (R36S)";
 
-void load_commands(const char *filename)
+void loadCommands(const char *filename)
 {
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
@@ -139,15 +144,16 @@ void updateRender(int selectedItem, SDL_Color color, SDL_Color highlightColor)
 	SDL_RenderClear(renderer);
 
 	// menu items
-	SDL_Surface *surface[numCommands];
-	SDL_Texture *texture[numCommands];
 	for (int i = 0; i < numCommands; i++) {
-		char str1[MAX_NAME_LENGTH] = "> ";
-		surface[i] = TTF_RenderText_Blended(mFont, selectedItem == i ? strcat(str1, commands[i].name) : commands[i].name,
-						    selectedItem == i ? highlightColor : color);
-		texture[i] = SDL_CreateTextureFromSurface(renderer, surface[i]);
-		SDL_Rect rect = { 20, 80 + i * 40, surface[i]->w, surface[i]->h };
-		SDL_RenderCopy(renderer, texture[i], NULL, &rect);
+		char itemName[MAX_NAME_LENGTH] = "> ";
+		SDL_Surface *surface =
+			TTF_RenderText_Blended(mFont, selectedItem == i ? strcat(itemName, commands[i].name) : commands[i].name,
+					       selectedItem == i ? highlightColor : color);
+		SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_Rect rect = { 20, 80 + i * 40, surface->w, surface->h };
+		SDL_RenderCopy(renderer, texture, NULL, &rect);
+		SDL_FreeSurface(surface);
+		SDL_DestroyTexture(texture);
 	}
 
 	// title
@@ -155,37 +161,29 @@ void updateRender(int selectedItem, SDL_Color color, SDL_Color highlightColor)
 	SDL_Texture *titleTexture = SDL_CreateTextureFromSurface(renderer, titleSurface);
 	SDL_Rect titleText_rect = { 20, 10, titleSurface->w, titleSurface->h };
 	SDL_RenderCopy(renderer, titleTexture, NULL, &titleText_rect);
-
+	SDL_FreeSurface(titleSurface);
+	SDL_DestroyTexture(titleTexture);
 	// battery
 	SDL_Surface *batterySurface = TTF_RenderText_Blended(sFont, batteryCapacityDisplayString, color);
 	SDL_Texture *batteryTexture = SDL_CreateTextureFromSurface(renderer, batterySurface);
 	SDL_Rect batteryRect = { 530, 5, batterySurface->w, batterySurface->h };
 	SDL_RenderCopy(renderer, batteryTexture, NULL, &batteryRect);
-
+	SDL_FreeSurface(batterySurface);
+	SDL_DestroyTexture(batteryTexture);
 	// Credit
 	SDL_Surface *creditSurface = TTF_RenderText_Blended(xsFont, creditDisplayString, color);
 	SDL_Texture *creditTexture = SDL_CreateTextureFromSurface(renderer, creditSurface);
 	SDL_Rect creditRect = { 20, 445, creditSurface->w, creditSurface->h };
 	SDL_RenderCopy(renderer, creditTexture, NULL, &creditRect);
+	SDL_FreeSurface(creditSurface);
+	SDL_DestroyTexture(creditTexture);
 
 	// render all
 	SDL_RenderPresent(renderer);
-
-	// free all
-	for (int i = 0; i < numCommands; i++) {
-		SDL_FreeSurface(surface[i]);
-		SDL_DestroyTexture(texture[i]);
-	}
-	SDL_FreeSurface(titleSurface);
-	SDL_DestroyTexture(titleTexture);
-	SDL_FreeSurface(batterySurface);
-	SDL_DestroyTexture(batteryTexture);
-	SDL_FreeSurface(creditSurface);
-	SDL_DestroyTexture(creditTexture);
 }
 
 // Function to execute a shell script
-void execute_shell_script(const char *script)
+void executeShellScript(const char *script)
 {
 	// Close SDL window
 	SDL_DestroyWindow(window);
@@ -199,9 +197,17 @@ void execute_shell_script(const char *script)
 
 	// Recreate SDL window
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
-	window = SDL_CreateWindow("Simple Launcher", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	joystick = SDL_JoystickOpen(0);
+}
+
+int eventFilterCallback(void *userdata, SDL_Event *event)
+{
+	if (event->type == SDL_MOUSEMOTION || event->type == SDL_JOYAXISMOTION) {
+		return SDL_FALSE; // Filter out (ignore) the event
+	}
+	return SDL_TRUE; // Allow all other events
 }
 
 int main(int argc, char *argv[])
@@ -212,19 +218,19 @@ int main(int argc, char *argv[])
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
 	TTF_Init();
 
-	window = SDL_CreateWindow("DYI Launcher", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 	if (window == NULL) {
 		printf("Could not create window: %s\n", SDL_GetError());
 		return 1;
 	}
 
 	renderer = SDL_CreateRenderer(window, -1, 0);
-	xsFont = TTF_OpenFont("./simple-launcher.ttf", 16);
-	sFont = TTF_OpenFont("./simple-launcher.ttf", 20);
-	mFont = TTF_OpenFont("./simple-launcher.ttf", 24);
-	lFont = TTF_OpenFont("./simple-launcher.ttf", 28);
-	xlFont = TTF_OpenFont("./simple-launcher.ttf", 32);
-	titleFont = TTF_OpenFont("./simple-launcher.ttf", 44);
+	xsFont = TTF_OpenFont(FONT_PATH, 16);
+	sFont = TTF_OpenFont(FONT_PATH, 20);
+	mFont = TTF_OpenFont(FONT_PATH, 24);
+	lFont = TTF_OpenFont(FONT_PATH, 28);
+	xlFont = TTF_OpenFont(FONT_PATH, 32);
+	titleFont = TTF_OpenFont(FONT_PATH, 44);
 	if (xsFont == NULL || sFont == NULL || mFont == NULL || lFont == NULL || xlFont == NULL || titleFont == NULL) {
 		printf("Failed to load font: %s\n", TTF_GetError());
 		return 1;
@@ -232,7 +238,7 @@ int main(int argc, char *argv[])
 
 	joystick = SDL_JoystickOpen(0);
 
-	load_commands("./simple-launcher-commands.txt");
+	loadCommands(COMMANDS_FILE);
 
 	SDL_Event event;
 	int running = 1;
@@ -242,10 +248,15 @@ int main(int argc, char *argv[])
 
 	updateRender(selectedItem, color, highlightColor);
 
+	SDL_SetEventFilter(eventFilterCallback, NULL);
+
 	// main loop
 	while (running) {
-		while (SDL_PollEvent(&event)) {
-			printf("Event type: %d\n", event.type);
+		// while (SDL_PollEvent(&event)) {
+		// if (SDL_WaitEventTimeout(&event, 20) != 0) {
+		if (SDL_WaitEvent(&event) != 0) {
+			// Events filtered by EventFilterCallback will not be processed here
+			// printf("Event type: %d\n", event.type);
 
 			switch (event.type) {
 			case SDL_QUIT:
@@ -268,7 +279,7 @@ int main(int argc, char *argv[])
 						selectedItem = 0;
 					break;
 				case SDLK_RETURN:
-					execute_shell_script(commands[selectedItem].command);
+					executeShellScript(commands[selectedItem].command);
 					break;
 				}
 				// R36S Special keys
@@ -282,6 +293,7 @@ int main(int argc, char *argv[])
 					break;
 				}
 				break;
+			/*
 			case SDL_JOYAXISMOTION:
 				if (event.jaxis.axis == 1) { // Y axis
 					if (event.jaxis.value < -10000) { // Up
@@ -297,6 +309,7 @@ int main(int argc, char *argv[])
 					}
 				}
 				break;
+			*/
 			case SDL_JOYBUTTONDOWN:
 				SDL_JoystickUpdate();
 				if (SDL_JoystickGetButton(joystick, 16)) { // Fn button is being pressed down
@@ -308,7 +321,7 @@ int main(int argc, char *argv[])
 				// 1:A, 0:B, 2:X, 3:Y, 8: Up, 9: Down, 10: Left, 11: Right, 12: Select, 13: Start, 16: Fn, 14: L3, 15: R3, 4: L1, 5: R1, 6: L2, 7: R2
 				switch (event.jbutton.button) {
 				case 1: // A
-					execute_shell_script(commands[selectedItem].command);
+					executeShellScript(commands[selectedItem].command);
 					break;
 				case 0: // B
 					selectedItem = 0;
@@ -331,6 +344,11 @@ int main(int argc, char *argv[])
 			}
 		}
 
+		updateRender(selectedItem, color, highlightColor);
+		SDL_Delay(10);
+		// FIXME: This is a workaround to prevent delay in updating the screen
+		updateRender(selectedItem, color, highlightColor);
+		SDL_Delay(10);
 		updateRender(selectedItem, color, highlightColor);
 	}
 
